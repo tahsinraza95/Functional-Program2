cbd9fb1afc4915802f4f3516d2951786f480ac05
tahsinraza95 <tahsinr693@gmail.com>
2023-02-06 05:11:13 +0530

    Updated: Result

---
+++ factorial/factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number for factorial");
+            int n = int.Parse(Console.ReadLine());
+            int value = 1;
+            while (n>0)
+            {
+                value = value * n;
+                n--;
+            }
+            Console.WriteLine(value);
+        }
+    }
+}
